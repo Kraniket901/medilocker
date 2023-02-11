318e63c9d4b1e14a06ff4cbb843cfdc7b86376d3
Viraj Chandra <virajchandra51@gmail.com>
2023-02-11 14:56:28 +0530

    log/sign

--- medilocker/src/pages/Login.jsx
+++ medilocker/src/pages/Login.jsx
@@ -1,77 +1,110 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import Web3 from "web3";
-import contract from '../contracts/cruds.json';
-import { useCookies } from 'react-cookie';
-import './Login.css'
+import contract from "../contracts/cruds.json";
+import { useCookies } from "react-cookie";
+import "./Login.css";
 
 const Login = () => {
+  const [log, setLog] = useState({
+    mail: "",
+    password: "",
+  });
 
-    const [log, setLog] = useState({
-        "mail": "",
-        "password": ""
+  const [cookies, setCookie] = useCookies();
+
+  function handle(e) {
+    const newData = { ...log };
+    newData[e.target.name] = e.target.value;
+    setLog(newData);
+  }
+
+  async function login() {
+    console.log(log);
+    var accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
     });
+    var currentaddress = accounts[0];
 
-    const [cookies, setCookie] = useCookies();
-
-    function handle(e) {
-        const newData = { ...log };
-        newData[e.target.name] = e.target.value;
-        setLog(newData);
-    }
-
-    async function login() {
-        console.log(log);
-        var accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        var currentaddress = accounts[0];
-
-        const web3 = new Web3(window.ethereum);
-        const mycontract = new web3.eth.Contract(contract['abi'], contract['networks']['5777']['address']);
-        // console.log(mycontract);
-        mycontract.methods.getdata().call().then(res => {
-            let flag = 0;
-            for (let i = 0; i < res.length; i++) {
-                const d = JSON.parse(res[i]);
-                if (d['type'] === 'user') {
-                    if (d['mail'] === log['mail']) {
-                        flag = 1;
-                        if (d['password'] === log['password']) {
-                            setCookie('index', i);
-                            setCookie('mail', d['mail']);
-                            setCookie('name', d['name']);
-                            setCookie('password', d['password']);
-                            window.location.href = "/myprofile";
-                        }
-                        else {
-                            alert("wrong password");
-                            return;
-                        }
-                    }
-                }
+    const web3 = new Web3(window.ethereum);
+    const mycontract = new web3.eth.Contract(
+      contract["abi"],
+      contract["networks"]["5777"]["address"]
+    );
+    // console.log(mycontract);
+    mycontract.methods
+      .getdata()
+      .call()
+      .then((res) => {
+        let flag = 0;
+        for (let i = 0; i < res.length; i++) {
+          const d = JSON.parse(res[i]);
+          if (d["type"] === "user") {
+            if (d["mail"] === log["mail"]) {
+              flag = 1;
+              if (d["password"] === log["password"]) {
+                setCookie("index", i);
+                setCookie("mail", d["mail"]);
+                setCookie("name", d["name"]);
+                setCookie("password", d["password"]);
+                window.location.href = "/myprofile";
+              } else {
+                alert("wrong password");
+                return;
+              }
             }
+          }
+        }
 
-            if (flag == 0) {
-                alert("Please create your account first");
-            }
-        })
-    }
+        if (flag == 0) {
+          alert("Please create your account first");
+        }
+      });
+  }
 
-    return (
-        <div  className="parent-form">
-        <div className="auth-form-container">
-            <h2>Login</h2>
-            <form className="login-form">
-                <label htmlFor="email">email</label>
-                <input onChange={(e) => handle(e)} type="email" placeholder="youremail@gmail.com" id="email" name="mail" />
-                <label htmlFor="password">password</label>
-                <input onChange={(e) => handle(e)} type="password" placeholder="********" id="password" name="password" />
-                <input className="log-btn" type="button" value="Log In" onClick={login} />
-            </form>
-
-            <a className="yo" href="/signup">Don't have an account? Register here.</a>
+  return (
+    <div className="login-container">
+      <form className="login-form">
+        <h2 className="login-form-title">Log In</h2>
+        <div className="input-container">
+          <div className="input-div">
+            <div className="input-heading">
+              <i className="fas fa-user"></i>
+              <h5>Email</h5>
+            </div>
+            <input
+              onChange={(e) => handle(e)}
+              type="email"
+              placeholder="youremail@gmail.com"
+              id="email"
+              name="mail"
+            />
+          </div>
+          <div className="input-div">
+            <div className="input-heading">
+              <i className="fas fa-lock"></i>
+              <h5>Password</h5>
+            </div>
+            <input
+              onChange={(e) => handle(e)}
+              type="password"
+              placeholder="********"
+              id="password"
+              name="password"
+            />
+          </div>
+          <p style={{ textAlign: "right" }}>Forgot password?</p>
         </div>
-        </div>
-    )
-}
+
+        <input
+          type="submit"
+          className="btn"
+          value="Log In"
+          onClick={login}
+        ></input>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
