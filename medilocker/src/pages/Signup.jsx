8cb3e1cc81a9eda44882e39ab62cbf51b3083ff2
Piyush Kumar <piyush.at.googl@gmail.com>
2023-02-11 11:11:52 +0530

    folder structure changed for blockchain

---
+++ medilocker/src/pages/Signup.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import './Signup.css'
+import Web3 from "web3";
+import contract from '../contracts/cruds.json';
+
+const Signup = () => {
+    const [reg, setReg] = useState({
+        "filled": "0",
+        "type": "user",
+        "name": "",
+        "mail": "",
+        "password": ""
+    });
+
+    function handle(e) {
+        const newData = { ...reg };
+        newData[e.target.name] = e.target.value;
+        setReg(newData);
+    }
+
+    async function register() {
+        var accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        var currentaddress = accounts[0];
+
+        const web3 = new Web3(window.ethereum);
+        const mycontract = new web3.eth.Contract(contract['abi'], contract['networks']['5777']['address']);
+        // console.log(mycontract);
+        mycontract.methods.addData(JSON.stringify(reg)).send({ from: currentaddress }).then(() => {
+            alert("Account created");
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Register</h2>
+            <form className="register-form">
+                <label htmlFor="name">Full name</label>
+                <input name="name" onChange={(e) => handle(e)} id="name" placeholder="full Name" />
+                <label htmlFor="email">email</label>
+                <input onChange={(e) => handle(e)} type="mail" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">password</label>
+                <input onChange={(e) => handle(e)} type="password" placeholder="********" id="password" name="password" />
+                <Link to="/myprofile">
+                <button type="submit" onClick={register}>Log In</button>
+                </Link>
+            </form>
+            {/* <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button> */}
+        </div>
+    )
+}
+
+export default Signup;
